834e44b2f601d472f59004c93227cc5d4e3ec7a3
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2023-01-06 22:24:40 +0200

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import UserResults from '../components/users/UserResults'
+
+function Home() {
+  return (
+    <>
+      {/* SEARCH COMPONENT */}
+      <UserResults />
+    </>
+  )
+}
+export default Home
